b1022b6da02b4b589fa288a86694b5896cc9391b
Shubham Gupta <shubhamgupta6022@gmail.com>
2025-03-18 22:24:03 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MovieHub"
+include(":app")
+include(":data")
+include(":domain")
+include(":feature")
+include(":feature:composite")
+include(":feature:core")
+include(":data:network")
+include(":data:composite")
